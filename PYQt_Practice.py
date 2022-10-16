0d40ca67188e1affb6325d55d2ffb2b87e4362f3
SiHoonChris <chrisleegpock@gmail.com>
2022-10-16 16:24:27 +0900

    2nd, 16:24, 10-16-2022

--- PYQt_Practice.py
+++ PYQt_Practice.py
@@ -5,6 +5,7 @@
 # 학습자료2 (초보자를 위한 Python GUI 프로그래밍 - PyQt5)
 # https://wikidocs.net/book/2944
 # 학습자료3 (파이썬으로 배우는 알고리즘 트레이딩 (개정판-2쇄))
+# 로그인할 때 에러 발생 원인 확인 후, [3) 기본 정보 요청하기]부터 시작
 # https://wikidocs.net/4236
 # 학습자료4 (키움 OpenAPI+ 파이썬 개발가이드)
 # https://wikidocs.net/book/8107
@@ -22,34 +23,22 @@ class MyWindow(QMainWindow):
         self.setWindowTitle("Main Window")
         self.setGeometry(300, 300, 300, 150)
 
-        # 파이썬에서 키움증권의 클래스 사용하려면 'QAxWidget' 사용해 객체 생성해야 함
-        # 키움증권 제공 클래스는 고유의 CLSID(16진수) 또는 ProgID(문자열) 가짐, 이를 'QAxWidget'의 생성자로 전달하면 인스턴스 생성
-        # 당연히 ProgID 활용하는 것이 편리함 
-        # 레지스트리 편집기 활용, 검색
-        # > ProgID : KHOPENAPI.KHOpenAPICtrl.1
-        # > CLSID : {A1574A0D-6BFA-4BD7-9020-DED88711818D}
         self.kiwoom=QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.kiwoom.dynamicCall("CommConnect()")
 
-        btn1=QPushButton("LOG-IN", self)
-        btn1.move(20, 20)
-        btn1.clicked.connect(self.btn1_clicked)
-    
-        btn2=QPushButton("Check State", self)
-        btn2.move(20, 70)
-        btn2.clicked.connect(self.btn2_clicked)
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setGeometry(10, 60, 280, 80)
+        self.text_edit.setEnabled(False) # 읽기/쓰기 모드
 
-    def btn1_clicked(self):
-        # CommConnect : OpenAPI가 제공하는 메서드(로그인 윈도우 실행)
-        # CommConnect 메서드 이용하려면 kiwoom(객체) 사용하여 dynamicCall 메서드 호출
-        # ( OCX방식에서의 호출 방식임 )
-        ret = self.kiwoom.dynamicCall("CommConnect")
+        # 로그인 성공 시, OnEventConnect 이벤트 발생
+        # 이를 활용해 GetConnectState 쓰지 않고 로그인 성공 여부 확인
+        # 이벤트(로그인) 이벤트 처리 메서드 연결
+        self.kiwoom.OnEventConnect.connect(self.event_connect)
 
-    def btn2_clicked(self):
-        # GetConnectState : OpenAPI가 제공하는 메서드(서버 접속상태 확인)
-        if self.kiwoom.dynamicCall("GetConnectState()")==0:
-            self.statusBar().showMessage("Not connected")
-        else:
-            self.statusBar().showMessage("Connected")
+    # 이벤트 처리 메서드
+    def event_connect(self, err_code):
+        if err_code == 0:
+            self.text_edit.append("로그인 성공")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
